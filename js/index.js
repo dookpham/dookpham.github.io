39185da30ac2ce8b4aa94a9292b308d5ade2854a
Duke Pham <dookpham@gmail.com>
2016-01-22 01:28:12 +0000

    added articles

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 var nameBtn = document.getElementById("name");
 var projectBtn = document.getElementById("projects");
+var articlesBtn = document.getElementById("articles");
 var nameGroup = document.getElementById("about-group");
 var projectGroup = document.getElementById("project-group");
+var articlesGroup = document.getElementById("articles-group");
 var currentTab = "name";
 
 nameBtn.style.color = "#EEE";
 projectGroup.style.display = "none";
+articlesGroup.style.display = "none";
 
 nameBtn.addEventListener("click", function() {
   if (currentTab != "name") {
     currentTab = "name";
     projectGroup.style.display = "none";
     nameGroup.style.display = "";
+    articlesGroup.style.display = "none";
+    articlesBtn.style.color = "";
     nameBtn.style.color = "#EEE";
     projectBtn.style.color = "";
   }
@@ -22,7 +27,21 @@ projectBtn.addEventListener("click", function() {
     currentTab = "project";
     projectGroup.style.display = "";
     nameGroup.style.display = "none";
+    articlesGroup.style.display = "none";
+    articlesBtn.style.color = "";
     nameBtn.style.color = "";
     projectBtn.style.color = "#EEE";
+  }
+});
+
+articlesBtn.addEventListener("click", function() {
+  if (currentTab != "articles") {
+    currentTab = "articles";
+    articlesGroup.style.display = "";
+    projectGroup.style.display = "none";
+    nameGroup.style.display = "none";
+    articlesBtn.style.color = "#EEE";
+    nameBtn.style.color = "";
+    projectBtn.style.color = "";
   }
 });
